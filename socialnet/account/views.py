f577fd2ba690612937d6cc5f7de87aa1e606312e
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-03-09 20:28:37 +0330

    feat:regitser user

--- socialnet/account/views.py
+++ socialnet/account/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 
 class RegisterUser(View):
-
+    form_class = UserRegisterForm
 
     def get(self, request):
-        form = UserRegisterForm()
-        return render(request, "account/register.html", {'form': form})
+        form = self.form_class()
+        return render(request, "account/register.html", {"form": form})
 
     def post(self, request):
-        return render(request, "")
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(
+                username=cd["username"], email=cd["eamil"], password=cd["password"]
+            )
+            messages.success(request, "user register seccessful", "success")
+
+            return redirect("home:home")
+        return render(request, "account/register.html", {"form": form})
--- socialnet/home/urls.py
+++ socialnet/home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 
-name = "home"
+app_name = "home"
 urlpatterns =[path("", views.HomeView.as_view(), name="home")] 
