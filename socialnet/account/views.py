cfa9eceef723033ce15bc02035ffa500968bd187
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-03-10 21:45:22 +0330

    feat:valid email

--- socialnet/account/views.py
+++ socialnet/account/views.py
@@ -19,9 +19,7 @@ class RegisterUser(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(
-                username=cd["username"], email=cd["eamil"], password=cd["password"]
-            )
+            User.objects.create_user(cd["username"], cd["email"], cd["password"])
             messages.success(request, "user register seccessful", "success")
 
             return redirect("home:home")
