51bf4603d734094dd83635760c8ceeb985ebb554
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-03-11 22:00:57 +0330

    feat:clean for password

--- socialnet/account/forms.py
+++ socialnet/account/forms.py
@@ -9,14 +9,28 @@ class UserRegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
     password = forms.CharField(
+        label="passwword",
         widget=forms.PasswordInput(
             attrs={"class": "form-control", "placeholder": "your password"}
-        )
+        ),
+    )
+    password_confirm = forms.CharField(
+        label="confirm",
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "your password"}
+        ),
     )
 
     def clean_email(self):
         email = self.cleaned_data["email"]
-        user = User.objects.filter(email=email).exists() #return true or false
+        user = User.objects.filter(email=email).exists()  # return true or false
         if user:
             raise ValidationError("this emial exist !")
         return email
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get("password")
+        p2 = cd.get("password_confirm")
+        if p1 and p2 and p1 != p2:
+            raise ValidationError("password most be like together")
